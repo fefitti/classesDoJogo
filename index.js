35ba9486c05ead7bada2f50ef7369beeaee7c80b
Fernanda Fittipaldi Santos <111749940+fefitti@users.noreply.github.com>
2024-05-27 12:02:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// Definindo a classe Heroi
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    // Método para o herói atacar
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'magia';
+                break;
+            case 'guerreiro':
+                ataque = 'espada';
+                break;
+            case 'monge':
+                ataque = 'artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'shuriken';
+                break;
+            default:
+                ataque = 'ataque desconhecido';
+        }
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Instanciando um herói
+let heroi1 = new Heroi('Aragorn', 30, 'guerreiro');
+
+// Chamando o método atacar
+heroi1.atacar();
